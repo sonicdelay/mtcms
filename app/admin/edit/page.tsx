66ef13c0689800dcb38eeea715ed4eed1eab5aec
Sascha Heß <sascha@sonicdelay.net>
2026-08-19 13:26:15 +0200

    fix: a lot and file edit
    Signed-off-by: Sascha Heß <sascha@sonicdelay.net>

--- app/admin/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-const ZERO_UUID = "00000000-0000-4000-8000-000000000000";
-
-export default function EditIndexPage() {
-  redirect(`/admin/edit/${ZERO_UUID}`);
-}
--- app/engine/engine-scene-client.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const EngineScene = dynamic(() => import("./engine-scene"), { ssr: false });
-
-export default function EngineSceneClient() {
-  return <EngineScene />;
-}
